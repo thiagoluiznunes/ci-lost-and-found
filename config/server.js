eff44d37f6cd3fbf46959a9caf20ae3ada2d6280
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-03-03 22:52:38 -0300

    add cors permission

--- config/server.js
+++ config/server.js
@@ -2,10 +2,14 @@ const port = 4000;
 // BODY parse of requistion
 const bodyParser = require('body-parser');
 const express = require('express');
+const allowCors = require('./cors');
+const queryParser = require('express-query-int');
 const server = express();
 
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
+server.use(allowCors);
+server.use(queryParser());
 
 server.listen(process.env.PORT || port, () => {
   console.log('Listening on');
