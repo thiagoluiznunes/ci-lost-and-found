a1ad7cf0f7c5cdd925e7a34d28728aa32a2f6d8c
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-03-03 21:25:42 -0300

    update item schema

--- api/item/item.js
+++ api/item/item.js
@@ -2,8 +2,12 @@ const restful = require('node-restful');
 const mongoose = restful.mongoose;
 
 const itemSchema = new mongoose.Schema({
-  name: {type: String, required: true},
   type: {type: String, required: true},
-  location: {type: String, required: true}});
+  description: {type: String, required: true},
+  contact: {type: String, required: true},
+  data: {
+    day:{type: Number, min: 0, max: 31, required: true},
+    month: {type: Number, min: 1, max: 12, required: true}
+  }});
 
 module.exports = restful.model('Item', itemSchema);
