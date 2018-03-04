0d0d53eb5daef6703cb9ec160f7b9110d7139bdc
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-03-03 21:47:20 -0300

    update item schema

--- api/item/item.js
+++ api/item/item.js
@@ -5,9 +5,6 @@ const itemSchema = new mongoose.Schema({
   type: {type: String, required: true},
   description: {type: String, required: true},
   contact: {type: String, required: true},
-  data: {
-    day:{type: Number, min: 0, max: 31, required: true},
-    month: {type: Number, min: 1, max: 12, required: true}
-  }});
+  data: {type: String, required: true}});
 
 module.exports = restful.model('Item', itemSchema);
--- api/item/itemService.js
+++ api/item/itemService.js
@@ -1,7 +1,7 @@
 const Item = require('./item');
 
 // Create REST API, adds CRUD to Mongog's schema
-Item.methods(['get', 'post', 'put', 'delete']);
+Item.methods(['get', 'post']);
 
 // Return post/put methods updated
 Item.updateOptions({new: true, runValidators: true});
