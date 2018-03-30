38a8b412b85dab497fceb92b7e8f075a738c8af9
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-03-30 00:06:09 -0300

    update database messages

--- api/item/item.js
+++ api/item/item.js
@@ -2,10 +2,11 @@ const restful = require('node-restful');
 const mongoose = restful.mongoose;
 
 const itemSchema = new mongoose.Schema({
-  type: {type: String, required: true},
-  description: {type: String, required: true},
-  name: {type: String, required: true},
-  contact: {type: String, required: true},
-  date: {type: String, required: true}});
+  type: {type: String, required: [true, 'Informe a opção achado/perdido.']},
+  description: {type: String, required:
+    [true, 'Informe a descrição do objeto.']},
+  name: {type: String, required: [true, 'Informe seu nome.']},
+  contact: {type: String, required: [true, 'Informe seu contato.']},
+  date: {type: String, required: [true, 'Informe a data.']}});
 
 module.exports = restful.model('Item', itemSchema);
--- config/database.js
+++ config/database.js
@@ -3,3 +3,5 @@ module.exports =
   mongoose.connect('mongodb://thiagoluiznunes:thiagoluizti1606@ds153198.mlab.com:53198/db_ci-lost-and-found',
     {useMongoClient: true}
   );
+
+mongoose.Error.messages.general.required = 'O atributo {PATH} é obrigatório.';
