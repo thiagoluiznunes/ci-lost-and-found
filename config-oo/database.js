ffe3cd190927f00d4423fcc3e8c887f542987a76
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-04-03 22:58:29 -0300

    create class database

--- config-oo/database.js
+++ config-oo/database.js
@@ -1,5 +1,15 @@
-const mongoose = require('mongoose');
-module.exports =
-  mongoose.connect('mongodb://thiagoluiznunes:thiagoluizti1606@ds153198.mlab.com:53198/db_ci-lost-and-found',
-    {useMongoClient: true}
-  );
+'use strict';
+
+class DataBase {
+  constructor() {
+    const mongoose = require('mongoose');
+
+    this.dbConnection = () => {
+      return mongoose.connect('mongodb://thiagoluiznunes:thiagoluizti1606@ds153198.mlab.com:53198/db_ci-lost-and-found',
+        {useMongoClient: true}
+      );
+    };
+  }
+}
+
+module.exports = DataBase;
--- loader.js
+++ loader.js
@@ -3,13 +3,14 @@
 // const server = require('./config/server');
 const ServerClass = require('./config-oo/server');
 const RoutesClass = require('./config-oo/routes');
+const DataBase = require('./config-oo/database');
 
 let server = new ServerClass(4000);
 let routes = new RoutesClass(server.getServer());
+let db = new DataBase();
 
 routes.addAPIMethod('api/item/itemService', '/item');
-
 routes.startAPIRoutes();
 routes.startAPISearch();
 
-require('./config/database');
+db.dbConnection();
