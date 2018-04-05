650b8a5c112c3628ab7260611d893f1ef968a2f1
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-04-05 01:06:20 -0300

    update itemRoutes class

--- api/item/itemRoutes.js
+++ api/item/itemRoutes.js
@@ -1,3 +1,17 @@
-export default (router, querys) => {
-  router.get('/search-name/:name', querys.searchByName);
-};
+import express from 'express';
+import service from './itemService';
+import query from './itemQuerys';
+
+class ItemRoutes {
+  constructor() {
+    const router = new express.Router();
+
+    this.initRoutes = (server) => {
+      server.use('/api', router);
+      service.register(router, '/item');
+      router.get('/search-name/:name', query.searchByName);
+    };
+  }
+}
+
+export default ItemRoutes;
--- loader.js
+++ loader.js
@@ -1,9 +1,10 @@
 import ServerClass from './config-oo/server';
-import ItemRoutes from './config-oo/routes/item-routes';
+import ItemRoutes from './api/item/itemRoutes';
 import DataBase from './config-oo/database';
 
 const server = new ServerClass(4000);
-const item = new ItemRoutes(server.getServer());
+const item = new ItemRoutes();
 const db = new DataBase();
 
+item.initRoutes(server.getServer());
 db.dbConnection();
