9a685661dd4be2537a7208dbab01c167549c85fc
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-04-05 00:49:45 -0300

    add class to schema

--- api/item/item.js
+++ api/item/item.js
@@ -2,11 +2,15 @@ import restful from 'node-restful';
 
 const mongoose = restful.mongoose;
 
-const itemSchema = new mongoose.Schema({
-  type: { type: String, required: [true, 'Informe a opção achado/perdido.'] },
-  description: { type: String, required: [true, 'Informe a descrição do objeto.'] },
-  name: { type: String, required: [true, 'Informe seu nome.'] },
-  contact: { type: String, required: [true, 'Informe seu contato.'] },
-  date: { type: String, required: [true, 'Informe a data.'] } });
+class Item extends mongoose.Schema {
+  constructor() {
+    super({
+      type: { type: String, required: [true, 'Informe a opção achado/perdido.'] },
+      description: { type: String, required: [true, 'Informe a descrição do objeto.'] },
+      name: { type: String, required: [true, 'Informe seu nome.'] },
+      contact: { type: String, required: [true, 'Informe seu contato.'] },
+      date: { type: String, required: [true, 'Informe a data.'] } });
+  }
+}
 
-export default restful.model('Item', itemSchema);
+export default restful.model('Item', new Item());
