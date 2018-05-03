d7895e3a7a4be127a6d5f5153de0fd3decfbd65b
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-05-03 01:56:26 -0300

    refator code smells

--- api/user/signup.js
+++ api/user/signup.js
@@ -3,28 +3,22 @@ import User from './user';
 
 class SignUp {
   constructor() {
-    this.signup = (req, res) => {
-      const emailRegex = /\S+@\S+\.\S+/;
-      const passwordRegex = /.{6,12}/;
+    const emailRegex = /\S+@\S+\.\S+/;
+    const passwordRegex = /.{6,12}/;
 
+    this.signup = (req, res) => {
       const name = req.body.name || '';
       const email = req.body.email || '';
       const password = req.body.password || '';
       const confirmPassword = req.body.confirm_password || '';
 
-      if (!email.match(emailRegex)) {
-        return res.status(400).send({ message: 'Email inválido!' });
-      }
-      if (!password.match(passwordRegex)) {
-        return res.status(400).send({ message: 'A senha deve conter entre 6 a 12 elementos!' });
-      }
+      if (!email.match(emailRegex)) return res.status(400).send({ message: 'Email inválido!' });
+      if (!password.match(passwordRegex)) return res.status(400).send({ message: 'A senha deve conter entre 6 a 12 elementos!' });
 
       const salt = bcrypt.genSaltSync();
       const passwordHash = bcrypt.hashSync(password, salt);
 
-      if (!bcrypt.compareSync(confirmPassword, passwordHash)) {
-        return res.status(400).send({ message: 'Senhas não conferem!' });
-      }
+      if (!bcrypt.compareSync(confirmPassword, passwordHash)) return res.status(400).send({ message: 'Senhas não conferem!' });
 
       User.findOne({ email }, (err, user) => {
         if (err) return res.status(400).send({ message: 'Erro de busca!' });
@@ -32,9 +26,7 @@ class SignUp {
 
         const newUser = new User({ name, email, password: passwordHash });
         newUser.save((err) => {
-          if (err) {
-            return res.status(400).send({ message: err });
-          }
+          if (err) return res.status(400).send({ message: err });
           return res.status(200).send({ data: true, message: 'Registro realizado com sucesso!' });
         });
       });
