6c752df15dd3f15b418b762c9a58be4dfed0013f
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-05-15 01:07:17 -0300

    update signup function

--- api/user/signup.js
+++ api/user/signup.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import bcrypt from 'bcrypt';
 import User from './user';
 
@@ -6,28 +7,34 @@ class SignUp {
     const emailRegex = /\S+@\S+\.\S+/;
     const passwordRegex = /.{6,12}/;
 
+    const sendErrorsFromDB = (res, dbErrors) => {
+      const errors = [];
+      _.forIn(dbErrors.errors, error => errors.push(error.message));
+      return res.status(400).json({ errors });
+    };
+
     this.signup = (req, res) => {
       const name = req.body.name || '';
       const email = req.body.email || '';
       const password = req.body.password || '';
       const confirmPassword = req.body.confirm_password || '';
 
-      if (!email.match(emailRegex)) return res.status(400).send({ message: 'Email inválido!' });
-      if (!password.match(passwordRegex)) return res.status(400).send({ message: 'A senha deve conter entre 6 a 12 elementos!' });
+      if (!email.match(emailRegex)) return res.status(400).send({ errors: ['Email inválido!'] });
+      if (!password.match(passwordRegex)) return res.status(400).send({ errors: ['A senha deve conter entre 6 a 12 elementos!'] });
 
       const salt = bcrypt.genSaltSync();
       const passwordHash = bcrypt.hashSync(password, salt);
 
-      if (!bcrypt.compareSync(confirmPassword, passwordHash)) return res.status(400).send({ message: 'Senhas não conferem!' });
+      if (!bcrypt.compareSync(confirmPassword, passwordHash)) return res.status(400).send({ errors: ['Senhas não conferem!'] });
 
       User.findOne({ email }, (err, user) => {
-        if (err) return res.status(400).send({ message: 'Erro de busca!' });
-        else if (user) return res.status(400).send({ errors: 'Usuário já cadastrado!' });
+        if (err) return sendErrorsFromDB(res, err);
+        else if (user) return res.status(400).send({ errors: ['Usuário já cadastrado!'] });
 
         const newUser = new User({ name, email, password: passwordHash });
         newUser.save((err) => {
-          if (err) return res.status(400).send({ message: err });
-          return res.status(200).send({ data: true, message: 'Registro realizado com sucesso!' });
+          if (err) return sendErrorsFromDB(res, err);
+          return res.status(200).send({ data: true, errors: ['Registro realizado com sucesso!'] });
         });
       });
     };
