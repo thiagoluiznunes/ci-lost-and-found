06f88f0b931d6605f81ab165009319199a799d4f
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-05-29 23:12:12 -0300

    update db module

--- config/database.js
+++ config/database.js
@@ -1,13 +1,9 @@
 import mongoose from 'mongoose';
 import bluebird from 'bluebird';
 
-class DataBase {
-  constructor() {
-    mongoose.Promise = bluebird;
-    mongoose.connect('mongodb://thiagoluiznunes:thiagoluizti1606@ds233739.mlab.com:33739/ci-database', { useMongoClient: true });
+const dbConnection = () => {
+  mongoose.Promise = bluebird;
+  mongoose.connect('mongodb://thiagoluiznunes:thiagoluizti1606@ds233739.mlab.com:33739/ci-database', { useMongoClient: true });
+};
 
-    this.dbConnection = () => mongoose;
-  }
-}
-
-export default DataBase;
+export default { dbConnection };
--- loader.js
+++ loader.js
@@ -1,14 +1,13 @@
 import ServerClass from './config/server';
 import ItemRoutes from './api/item/itemRoutes';
 import UserRoutes from './api/user/userRoutes';
-import DataBase from './config/database';
+import database from './config/database';
 import protect from './config/auth';
 
 const server = new ServerClass(4000);
 const item = new ItemRoutes();
 const user = new UserRoutes();
-const db = new DataBase();
 
 item.initRoutes(server.getServer(), protect);
 user.initRoutes(server.getServer());
-db.dbConnection();
+database.dbConnection();
