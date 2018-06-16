c032395e21f40cf8b224bba96167699f807dae34
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-06-16 12:19:06 -0300

    add adapter for database connection using design patterns

--- config/database.js
+++ config/database.js
@@ -1,9 +1,39 @@
 import mongoose from 'mongoose';
 import bluebird from 'bluebird';
 
-const dbConnection = () => {
+/**
+ * Adapter Design Patterns
+ */
+
+const mongoDBConnection = () => {
   mongoose.Promise = bluebird;
   mongoose.connect('mongodb://thiagoluiznunes:thiagoluizti1606@ds233739.mlab.com:33739/ci-database', { useMongoClient: true });
+  console.log('MongoDB connected!');
+};
+
+const mySQLConnection = () => {
+  console.log('MySQL connected!');
+};
+
+const postgreSQLConnection = () => {
+  console.log('PostgresSQL connected!');
+};
+
+const dbConnection = (type) => {
+  switch (type) {
+    case 'mongodb':
+      mongoDBConnection();
+      break;
+    case 'mysql':
+      mySQLConnection();
+      break;
+    case 'postgres':
+      postgreSQLConnection();
+      break;
+    default:
+      // statements_def
+      break;
+  }
 };
 
 export default { dbConnection };
