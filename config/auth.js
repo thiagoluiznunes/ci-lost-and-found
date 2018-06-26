5e0c203a65c7fb051e6a7655fba32b1d96fb62f0
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-06-26 01:41:00 -0300

    fix error can't set headers

--- config/auth.js
+++ config/auth.js
@@ -2,17 +2,19 @@ import jwt from 'jsonwebtoken';
 import env from '../dotenv';
 
 const protect = (req, res, next) => {
-  if (req.method === 'OPTIONS') next();
-
-  const authorization = 'authorization';
-  const token = req.body.token || req.query.token || req.headers[authorization];
-
-  if (!token) return res.status(403).send({ errors: ['Nenhum token fornecido.'] });
-
-  jwt.verify(token, env.authSecret, (err) => {
-    if (err) return res.status(403).send({ errors: ['Falha ao autenticar token.'] });
+  if (req.method === 'OPTIONS') {
     next();
-  });
+  } else {
+    const authorization = 'authorization';
+    const token = req.body.token || req.query.token || req.headers[authorization];
+
+    if (!token) return res.status(403).send({ errors: ['Nenhum token fornecido.'] });
+
+    jwt.verify(token, env.authSecret, (err) => {
+      if (err) return res.status(403).send({ errors: ['Falha ao autenticar token.'] });
+      next();
+    });
+  }
 };
 
 export default { protect };
